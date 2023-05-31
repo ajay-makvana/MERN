94e4a2654d2cabf14baea7155df2401953728686
ajay-makvana <ajaymakvana2001@gmail.com>
2023-05-31 19:21:49 +0530

    Initial Commit

---
+++ server/app.js
@@ -0,0 +1,54 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app  = express();
+
+dotenv.config({path:'./config.env'});
+
+const DB = process.env.DATABASE;
+const PORT = process.env.PORT;
+
+mongoose.connect(DB, 
+    {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false
+    }
+    ).then(() => {
+        console.log("connectiontion succesfull with DB");
+    }
+).catch((err)=> console.log(err));
+
+// Middleware
+
+const middleware = (req, res, next) => {
+    console.log("Hello from Middleware");
+    next();
+}
+
+// middleware();
+
+// Routing
+app.get('/', (req, res) => {
+    res.send('Hello from Server');
+});
+
+app.get('/about', middleware ,(req, res) => {
+    res.send('Hello from About');
+});
+app.get('/contact', (req, res) => {
+    res.send('Hello from Contact');
+});
+app.get('/signin', (req, res) => {
+    res.send('Hello from Signin');
+});
+app.get('/signup', (req, res) => {
+    res.send('Hello from Registration');
+});
+
+// console.log("Console Output");
+app.listen(3000, () => {
+    console.log(`Server Running at port ${PORT}`);
+}) 
