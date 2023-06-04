c629bbe442f13140c29ff154209e5609fb0a7f5d
ajay-makvana <ajaymakvana2001@gmail.com>
2023-06-04 15:10:45 +0530

    10 User Registration using async await method

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,37 +8,75 @@ router.get('/', (req, res) => {
     res.send('Hello from Server router.js');
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    // console.log(req.body.name+" "+req.body.email);
-    // res.json({message: req.body});
-    // res.send("Register from router");
+// Using Promise
+
+// router.post('/register', (req, res) => {
+//     console.log(req.body);
+//     // console.log(req.body.name+" "+req.body.email);
+//     // res.json({message: req.body});
+//     // res.send("Register from router");
+//     const { name, email, phone, work, password, cpassword } = req.body;
+//     // console.log(name+" "+email);
+//     if (!name || !email || !phone || !work || !password || !cpassword) {
+//         // return res.json({error:"fill all details properly"});
+//         return res.status(422).json({ error: "fill all details properly" });
+//     }
+
+//     // finding as any user with same email exist on database or not
+//     // database email: user filled email
+//     User.findOne({ email: email })
+//         .then((userExist) => {
+//             if (userExist) {
+//                 return res.status(422).json({ error: "Email already exist" });
+//             }
+
+//             // key value pair to store in database
+//             const user = new User({ name: name, email: email, phone: phone, work: work, password: password, cpassword: cpassword });
+//             user.save().then(() => {
+//                 return res.status(200).json({ error: "User created succesfully" });
+//             }).catch((err) => {
+//                 res.status(500).json({ error: "Failed to Register" });
+//             })
+
+//         }).catch((err => {
+//             console.log(err);
+//         }));
+
+// });
+
+
+// Using Async-Await 
+
+router.post('/register', async (req, res) => {
+
     const { name, email, phone, work, password, cpassword } = req.body;
-    // console.log(name+" "+email);
+
     if (!name || !email || !phone || !work || !password || !cpassword) {
-        // return res.json({error:"fill all details properly"});
         return res.status(422).json({ error: "fill all details properly" });
     }
 
-    // finding as any user with same email exist on database or not
-    // database email: user filled email
-    User.findOne({ email: email })
-        .then((userExist) => {
-            if (userExist) {
-                return res.status(422).json({ error: "Email already exist" });
-            }
+    try {
+        const userExist = await User.findOne({ email: email });
 
-            // key value pair to store in database
-            const user = new User({ name: name, email: email, phone: phone, work: work, password: password, cpassword: cpassword });
-            user.save().then(() => {
-                return res.status(200).json({ error: "User created succesfully" });
-            }).catch((err) => {
-                res.status(500).json({ error: "Failed to Register" });
-            })
+        if (userExist) {
+            return res.status(422).json({ error: "Email already exist" });
+        }
 
-        }).catch((err => {
-            console.log(err);
-        }));
+        // key value pair to store in database
+        const user = new User({ name: name, email: email, phone: phone, work: work, password: password, cpassword: cpassword });
+
+        const userRegistered = await user.save();
+
+        if (userRegistered) {
+            return res.status(200).json({ error: "User created succesfully" });
+        }
+        else {
+            res.status(500).json({ error: "Failed to Register" });
+        }
+    }
+    catch {
+        console.log(err);
+    }
 
 });
 
