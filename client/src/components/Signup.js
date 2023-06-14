4e5067995241880035e5aa1fbb85b6688655fcbc
ajay-makvana <ajaymakvana2001@gmail.com>
2023-06-14 22:41:49 +0530

    28 user Registration form data accessing through useState

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Signup = () => {
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        work: "",
+        password: "",
+        cpassword: ""
+    });
+
+
+    let name, value; //{ key, value } pair
+    const handleInputs = (e) => {
+        console.log(e);
+        name = e.target.name; // name of inputfield which is typing
+        value = e.target.value; // value of inputfield which is typing
+
+        // using spread operator required user all data
+        // here get dynamic data 'name'
+        setUser({ ...user, [name]: value });
+        // setting the name field value to value in 
+    }
+
     return (
         <>
             <section className='signup'>
@@ -16,42 +39,42 @@ const Signup = () => {
                                 <label htmlFor='name'>
                                     <i className='zmdi zmdi-account material-icons-name'></i>
                                 </label>
-                                <input type='text' name='name' id='name' autoComplete='off' placeholder='Your Name' />
+                                <input type='text' name='name' id='name' autoComplete='off' value={user.name} onChange={handleInputs} placeholder='Your Name' />
                             </div>
 
                             <div className='form-group'>
                                 <label htmlFor='email'>
                                     <i className='zmdi zmdi-email material-icons-name'></i>
                                 </label>
-                                <input type='email' name='email' id='email' autoComplete='off' placeholder='Your Email' />
+                                <input type='email' name='email' id='email' autoComplete='off' value={user.email} onChange={handleInputs} placeholder='Your Email' />
                             </div>
 
                             <div className='form-group'>
                                 <label htmlFor='phone'>
                                     <i className='zmdi zmdi-phone-in-talk material-icons-name'></i>
                                 </label>
-                                <input type='number' name='phone' id='phone' autoComplete='off' placeholder='Your Phone' />
+                                <input type='number' name='phone' id='phone' autoComplete='off' value={user.phone} onChange={handleInputs} placeholder='Your Phone' />
                             </div>
 
                             <div className='form-group'>
                                 <label htmlFor='work'>
                                     <i className='zmdi zmdi-slideshow material-icons-name'></i>
                                 </label>
-                                <input type='text' name='work' id='work' autoComplete='off' placeholder='Your Work' />
+                                <input type='text' name='work' id='work' autoComplete='off' value={user.work} onChange={handleInputs} placeholder='Your Work' />
                             </div>
 
                             <div className='form-group'>
                                 <label htmlFor='password'>
                                     <i className='zmdi zmdi-lock material-icons-name'></i>
                                 </label>
-                                <input type='password' name='password' id='password' autoComplete='off' placeholder='Your Password' />
+                                <input type='password' name='password' id='password' autoComplete='off' value={user.password} onChange={handleInputs} placeholder='Your Password' />
                             </div>
 
                             <div className='form-group'>
                                 <label htmlFor='cpassword'>
                                     <i className='zmdi zmdi-lock material-icons-name'></i>
                                 </label>
-                                <input type='password' name='cpassword' id='cpassword' autoComplete='off' placeholder='Confirm Password' />
+                                <input type='password' name='cpassword' id='cpassword' autoComplete='off' value={user.cpassword} onChange={handleInputs} placeholder='Confirm Password' />
                             </div>
 
                             <div className='form-group form-button'>
